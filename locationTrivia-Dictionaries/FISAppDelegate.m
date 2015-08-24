10c5dfc5a20c0d8f7a21a18c79e0f5749569319c
kevinjtsai <tsai.kevin.j@gmail.com>
2015-08-24 13:28:50 -0400

    finished lab

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -22,4 +22,61 @@
  
  */
 
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location
+                                     toLength:(NSUInteger)length{
+    if (length < [location[@"name"] length]) {
+        NSString *subString = [location[@"name"] substringToIndex:(length)];
+        return subString;
+    };
+    return location[@"name"];
+};
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name
+                                      latitude:(CGFloat)latitude
+                                     longitude:(CGFloat)longitude{
+    NSMutableDictionary *dictionary = [NSMutableDictionary dictionary];
+    dictionary[@"name"] = name;
+    dictionary[@"latitude"] = @(latitude);
+    dictionary[@"longitude"] = @(longitude);
+    
+    NSLog(@"%@", dictionary);
+    return dictionary;
+};
+
+-(NSArray *)namesOfLocations:(NSArray *)locations{
+    NSMutableArray *locationNamesArray = [NSMutableArray array];
+    for (id object in locations) {
+        [locationNamesArray addObject:object[@"name"]];
+    }
+    return locationNamesArray;
+};
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)location{
+
+    NSArray *arrayToMatch = @[@"name",
+                             @"latitude",
+                             @"longitude"];
+    
+    if([[[location allKeys] sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)] isEqualToArray:[arrayToMatch sortedArrayUsingSelector:@selector(caseInsensitiveCompare:)]]){
+        return YES;
+    }
+    return NO;
+};
+
+-(NSDictionary *)locationNamed:(NSString *)name
+                   inLocations:(NSArray *)locations{
+    NSLog(@"%@", locations);
+    NSDictionary *returnDictionary = [NSDictionary dictionary];
+    for (NSDictionary *location in locations){
+        if ([location[@"name"] isEqualToString:name]){
+            returnDictionary = location;
+        } if ([returnDictionary count]>0){
+            return returnDictionary;
+        } 
+    };
+    return nil;
+};
+
+
+
 @end

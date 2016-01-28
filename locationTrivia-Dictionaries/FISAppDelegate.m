34abe7473066f61bd00b3230294ced00c55f09e2
molgin <mollygingras@gmail.com>
2016-01-28 17:26:14 -0500

    analyze location info

--- locationTrivia-Dictionaries/FISAppDelegate.m
+++ locationTrivia-Dictionaries/FISAppDelegate.m
@@ -10,10 +10,40 @@
     return YES;
 }
 
-/** 
- 
- *  Write your method implementations here.
- 
- */
+-(NSString *)stringByTruncatingNameOfLocation:(NSDictionary *)location toLength:(NSUInteger)length{
+    NSString *name = location[@"name"];
+    if (length > [name length]) {
+        length = [name length];
+    }
+    return [name substringToIndex:length];
+}
+
+-(NSDictionary *)dictionaryForLocationWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude{
+    return @{@"name": name,
+             @"latitude": @(latitude),
+             @"longitude": @(longitude)};
+}
+
+-(NSArray *)namesOfLocations:(NSArray *)locations{
+    return [locations valueForKey:@"name"];
+}
+
+-(BOOL)dictionaryIsValidLocation:(NSDictionary *)dict{
+    NSSortDescriptor *sortAsc = [NSSortDescriptor sortDescriptorWithKey:@"self"
+                                                              ascending:YES
+                                                               selector:@selector(caseInsensitiveCompare:) ];
+    return [[[dict allKeys] sortedArrayUsingDescriptors:@[sortAsc]] isEqualToArray:[@[@"name", @"latitude", @"longitude"] sortedArrayUsingDescriptors:@[sortAsc]]];
+}
+
+-(NSArray *)locationNamed:(NSString *)name inLocations:(NSArray *)locations{
+    NSPredicate *namePredicate = [NSPredicate predicateWithFormat:@"name == %@", name];
+    NSArray *matchingLocations = [locations filteredArrayUsingPredicate:namePredicate];
+    if ([matchingLocations count]) {
+        return matchingLocations[0];
+    }
+    else {
+        return nil;
+    }
+}
 
 @end
